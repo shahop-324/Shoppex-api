49f9bbf200cf1abc6810782e0f55c069ef088fbe
SHREYANSH SHAH <60038350+shahop-324@users.noreply.github.com>
2022-02-18 10:22:37 +0530

    commit

--- model/referralModel.js
+++ model/referralModel.js
@@ -1,14 +1,5 @@
 const mongoose = require('mongoose')
 
-const earnings = new mongoose.Schema({
-  status: { type: String, enum: ['Not Paid', 'Paid'], default: 'Not Paid' },
-  amount: { type: Number },
-  orderAmount: { type: Number },
-  order: { type: mongoose.Schema.ObjectId, ref: 'Order' },
-  orderRef: {type: String,},
-  timestamp: { type: Date, default: Date.now() },
-})
-
 const referralSchema = new mongoose.Schema({
   store: {
     type: mongoose.Schema.ObjectId,
@@ -22,7 +13,6 @@ const referralSchema = new mongoose.Schema({
   commission: { type: Number },
   totalSales: { type: Number, default: 0 },
   totalEarnings: { type: Number, default: 0 },
-  earnings: [{ earnings }],
   createdAt: { type: Date, default: Date.now() },
   updatedAt: { type: Date },
 })
