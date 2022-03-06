61b12e6a9d26d2340d3e7a98689717a65e5a24e5
SHREYANSH SHAH <60038350+shahop-324@users.noreply.github.com>
2022-03-06 14:20:14 +0530

    commit

--- route/mobileRoutes.js
+++ route/mobileRoutes.js
@@ -1,22 +1,21 @@
-const express = require('express')
-const router = express.Router()
-const mobileController = require('../controllers/mobileController');
+const express = require("express");
+const router = express.Router();
+const mobileController = require("../controllers/mobileController");
 
-router.post('/login', mobileController.login);
+router.post("/login", mobileController.login);
 
+router.post("/verifyAndLogin", mobileController.verifyAndLogin);
 
+router.post("/resendLoginOTP", mobileController.resendLoginOTP);
 
-router.post('/verifyAndLogin', mobileController.verifyAndLogin);
+router.post(
+  "/register",
+  mobileController.register,
+  mobileController.resendRegisterOTP
+);
 
+router.post("/verifyAndRegister", mobileController.verifyAndRegister);
 
-
-router.post('/resendLoginOTP', mobileController.resendLoginOTP);
-
-
-router.post('/register', mobileController.register, mobileController.resendRegisterOTP);
-
-router.post('/verifyAndRegister', mobileController.verifyAndRegister);
-
-router.post('/resendRegisterOTP', mobileController.resendRegisterOTP);
+router.post("/resendRegisterOTP", mobileController.resendRegisterOTP);
 
 module.exports = router;
