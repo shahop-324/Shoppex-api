7d7240b24886c197cde8467a1f4015b0f8f07a0d
SHREYANSH SHAH <60038350+shahop-324@users.noreply.github.com>
2022-03-23 13:01:55 +0530

    commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -538,6 +538,7 @@ exports.protectAdmin = catchAsync(async (req, res, next) => {
 exports.protect = catchAsync(async (req, res, next) => {
   // 1) Getting token and check if it's there
   let token;
+
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
@@ -546,6 +547,8 @@ exports.protect = catchAsync(async (req, res, next) => {
   } else if (req.cookies.jwt) {
     token = req.cookies.jwt;
   }
+
+  console.log(token);
 
   if (!token) {
     return next(
