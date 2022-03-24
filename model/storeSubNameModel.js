69c50d06a22a2260dce13129f69479e858756322
SHREYANSH SHAH <60038350+shahop-324@users.noreply.github.com>
2022-03-24 19:10:31 +0530

    commit

--- model/storeSubNameModel.js
+++ model/storeSubNameModel.js
@@ -1,20 +1,22 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const storeSubNameSchema = new mongoose.Schema({
-    subName: {
-        type: String,
-    },
-    store: {
-        type: mongoose.Schema.ObjectId,
-        ref: "Store",
-    },
-    createdAt: { // unselect
-        type: Date,
-    },
-    updatedAt: { // unselect
-        type: Date,
-    },
+  subName: {
+    type: String,
+  },
+  store: {
+    type: mongoose.Schema.ObjectId,
+    ref: "Store",
+  },
+  createdAt: {
+    // unselect
+    type: Date,
+  },
+  updatedAt: {
+    // unselect
+    type: Date,
+  },
 });
 
-const StoreSubName = new mongoose.model('StoreSubName', storeSubNameSchema);
+const StoreSubName = new mongoose.model("StoreSubName", storeSubNameSchema);
 module.exports = StoreSubName;
